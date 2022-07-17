087bff12e8be2d525b5194dba3b6281664c6d001
dmishra613 <dmishra613@gmail.com>
2022-07-17 17:53:00 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './components/property/property-card/property-card.component';
+import { PropertyListComponent } from './components/property/property-list/property-list.component';
+import { NavBarComponent } from './components/property/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
